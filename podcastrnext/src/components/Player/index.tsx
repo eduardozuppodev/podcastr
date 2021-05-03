f48bab0b097a8126fa61e5f12ea19285d62e2c7e
eduardozuppodev <zuppo.edu@gmail.com>
2021-05-03 12:08:46 -0300

    .

--- podcastrnext/src/components/Player/index.tsx
+++ podcastrnext/src/components/Player/index.tsx
@@ -5,6 +5,9 @@ import Image from 'next/image';
 import { playerContext } from "../../contexts/PlayerContext";
 import Episode from "../../pages/episodes/[slug]";
 
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+
 import styles from "./styles.module.scss";
 
 export function Player() {
@@ -37,11 +40,20 @@ export function Player() {
                 </div>
             )}
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+                        {episode ? (
+                            <Slider
+                                trackStyle={{backgroundColor: '#04d361'}}
+                                railStyle={{backgroundColor: '#9f75ff'}}
+                                handleStyle={{borderColor: '#04d361', borderWidth: 4}}
+                            />
+                        ): (
+                            <div className = {styles.emptySlider} />
+
+                        )}
                     </div>
 
                     <span>00:00</span>
